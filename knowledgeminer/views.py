cd3350b2cf6c3abdb3a1fd08f6953aa03012ee2a
pockybrend <78880166+pockybrend@users.noreply.github.com>
2023-05-23 00:34:25 -0600

    views.py

--- knowledgeminer/views.py
+++ knowledgeminer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .scriptsMineria import eda as EDA
+from .scriptsMineria import pca as PCA
 
 # Create your views here.
 def say_hello(request):
@@ -17,3 +18,12 @@ def eda_prueba(request):
         'file': file_name,
     }
     return render(request = request, template_name='eda.html', context = context)
+
+def pca_prueba(request):
+    file_name = 'C:/Users/offue/Downloads/Hipoteca.csv'
+    pca = PCA.initialization(file_name)
+    context = {
+        'pca': pca,
+        'file': file_name,
+    }
+    return render(request = request, template_name = 'pca.html', context = context)
