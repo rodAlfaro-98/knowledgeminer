b9eae16b6ef5ebccf6d86eed8935ac28e9fa167e
pockybrend <78880166+pockybrend@users.noreply.github.com>
2023-05-23 11:22:52 -0600

    Update views.py

--- knowledgeminer/views.py
+++ knowledgeminer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .scriptsMineria import eda as EDA
-from .scriptsMineria import pca as PCA
+from .scriptsMineria import pca as PCAA
+from .scriptsMineria import ad as AD
 
 # Create your views here.
 def say_hello(request):
@@ -11,7 +12,7 @@ def say_hello(request):
     return render(request=request,template_name='hello.html',context=context)
 
 def eda_prueba(request):
-    file_name = 'E:/Windows/FI/carrera/DecimoSemestre/Mineria/practicas/practica1/assets/country_vaccinations.csv'
+    file_name = 'C:/Users/offue/Downloads/country_vaccinations.csv'
     eda = EDA.initialization(file_name)
     context = {
         'eda': eda,
@@ -21,9 +22,18 @@ def eda_prueba(request):
 
 def pca_prueba(request):
     file_name = 'C:/Users/offue/Downloads/Hipoteca.csv'
-    pca = PCA.initialization(file_name)
+    pca = PCAA.initialization(file_name)
     context = {
         'pca': pca,
         'file': file_name,
     }
     return render(request = request, template_name = 'pca.html', context = context)
+
+def ad_prueba(request):
+    file_name = 'C:/Users/offue/Downloads/diabetes.csv'
+    ad = AD.initialization(file_name)
+    context = {
+        'ad': ad,
+        'file': file_name,
+    }
+    return render(request = request, template_name = 'ad.html', context = context)
